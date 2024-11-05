00cce090969bc44096752036aa51e3786e371d09
sarathir-dev <sarathir.dev@gmail.com>
2024-11-05 12:31:38 +0530

    Added Vision Transformer (ViT) to model architecture

--- ViT.py
+++ ViT.py
@@ -143,3 +143,44 @@ class TransformerEncoder(nn.module):
         # residual connection after sub-layer 2
         out = out * self.mlp(self.ln2(out))
         return out
+
+
+# Vision Transformer
+class VisionTransformer(nn.module):
+    def __init__(self, d_model, n_classes, img_size, patch_size, n_channels, n_heads, n_layers):
+        super().__init__()
+
+        assert img_size[0] % patch_size[0] == 0 and img_size[1] % patch_size[
+            1] == 0, "img_size dimensions must be divisible by patch_size dimensions"
+        assert d_model % n_heads == 0, "d_model must be divisible by n_heads"
+
+        self.d_model = d_model  # dimensionality of model
+        self.n_classes = n_classes  # number of classes
+        self.img_size = img_size  # image size
+        self.patch_size = patch_size  # patch size
+        self.n_channels = n_channels  # number of channels
+        self.n_heads = n_heads  # number of channels
+
+        self.n_patches = (
+            self.img_size[0] * self.img_size[1]) // (self.patch_size[0] * self.patch_size[1])
+        self.max_seq_length = self.n_patches + 1
+
+        self.patch_embedding = PatchEmbeddings(
+            self.d_model, self.img_size, self.patch_size, self.n_channels)
+        self.postional_encoding = PositionalEncoding(
+            self.d_model, self.max_seq_length)
+        self.transformer_encoder = nn.Sequential(
+            *[TransformerEncoder(self.d_model, self.n_heads) for _ in range(n_layers)])
+
+        # classification MLP
+        self.classifier = nn.Sequential(
+            nn.Linear(self.d_model, self.n_classes),
+            nn.Softmax(dim=-1)
+        )
+
+    def forward(self, images):
+        x = self.patch_embedding(images)
+        x = self.postional_encoding(x)
+        x = self.transformer_encoder(x)
+        x = self.classifier(x[:, 0])
+        return x
